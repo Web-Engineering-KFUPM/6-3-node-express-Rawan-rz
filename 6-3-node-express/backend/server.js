8278415b05e26542359f82556de3efe240c1d556
Rawan20000 <rawan_rz@icloud.com>
2026-04-06 13:19:45 +0300

    TODO 4

--- 6-3-node-express/backend/server.js
+++ 6-3-node-express/backend/server.js
@@ -1,12 +1,13 @@
 // TODO 1: Import and Create express app instance
 import express from "express";
 const app = express();
-const PORT = 3000;
+
 // TODO 1: Define server port
 
-
+const PORT = 3000;
 // TODO 4: Import and Apply CORS middleware
-
+import cors from "cors";
+app.use(cors());
 
 // TODO 5: Import and apply Morgan middleware
 
